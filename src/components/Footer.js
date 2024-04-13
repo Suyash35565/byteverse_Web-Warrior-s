c22ff833f0f65dfb5bd66dffb1db855e50548888
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2024-04-13 22:12:33 +0530

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,33 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+import React from 'react';
+import './footer.css';
+import Logo1 from './assets/logo.jpg'
+import Logo2 from './assets/facebook.svg'
+import Logo3 from './assets/insta.svg'
+import Logo4 from './assets/linkedin.svg'
+import Logo5 from './assets/twitter.svg'
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-content">
+                <h3>
+                    <img className="flogo" src={Logo1} alt="logo" />
+                    <br />
+                    Venture Catalyst
+                </h3>
+                <div className="socials">
+                    <ul className="socials">
+                        <li><a href="https://www.google.com"><i className="facebook"> <img className="scon" src={Logo2} alt="facebook" /></i></a></li>
+                        <li><a href="https://www.google.com"><i className="twitter"> <img className="scon" src={Logo5} alt="twitter" /></i></a></li>
+                        <li><a href="https://www.google.com"><i className="insta"> <img className="scon" src={Logo3} alt="instagram" /></i></a></li>
+                        <li><a href="https://www.google.com"><i className="linkedin"> <img className="scon" src={Logo4} alt="linkedin" /></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>© 2024 Venture Catalyst | Privacy Policy | Terms & Conditions</p>
+            </div>
+        </footer>
+    );
 }
+
+export default Footer
