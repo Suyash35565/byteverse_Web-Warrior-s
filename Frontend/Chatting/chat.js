2b02109f7eedaa7f8cef101eced9848b2080c92e
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2024-04-14 19:35:27 +0530

    Add files via upload

---
+++ Frontend/Chatting/chat.js
@@ -0,0 +1,224 @@
+
+
+
+
+const userMessage = [
+    ["hi", "hey", "hello"],
+    ["sure", "yes", "no"],
+    ["are you genious", "are you nerd", "are you intelligent"],
+    ["i hate you", "i dont like you"],
+    ["how are you", "how is life", "how are things", "how are you doing"],
+    ["how is corona", "how is covid 19", "how is covid19 situation"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you", "who is your creator"],
+  
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself"
+    ],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool", "very good"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "ok", "okay", "nice", "welcome"],
+    ["thanks", "thank you"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["corona", "covid19", "coronavirus"],
+    ["you are funny"],
+    ["i dont know"],
+    ["boring"],
+    ["im tired"]
+  ];
+  const botReply = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!"],
+    ["Okay"],
+    ["Yes I am! "],
+    ["I'm sorry about that. But I like you dude."],
+    [
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?"
+    ],
+    ["Getting better. There?", "Somewhat okay!", "Yeah fine. Better stay home!"],
+  
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "Can you guess?",
+      "I don't know actually"
+    ],
+    ["I am always young."],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["Sabitha Kuppusamy"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV", "Chat with me."],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["You're welcome"],
+    ["Briyani", "Burger", "Sushi", "Pizza"],
+    ["Dude!"],
+    ["Yes?"],
+    ["Please stay home"],
+    ["Glad to hear it"],
+    ["Say something interesting"],
+    ["Sorry for that. Let's chat!"],
+    ["Take some rest, Dude!"]
+  ];
+  
+  const alternative = [
+    "Same here, dude.",
+    "That's cool! Go on...",
+    "Dude...",
+    "Ask something else...",
+    "Hey, I'm listening..."
+  ];
+  
+  const synth = window.speechSynthesis;
+  
+  function voiceControl(string) {
+    let u = new SpeechSynthesisUtterance(string);
+    u.text = string;
+    u.lang = "en-aus";
+    u.volume = 1;
+    u.rate = 1;
+    u.pitch = 1;
+    synth.speak(u);
+  }
+  
+  function sendMessage() {
+    const inputField = document.getElementById("input");
+    let input = inputField.value.trim();
+    input != "" && output(input);
+    inputField.value = "";
+  }
+  document.addEventListener("DOMContentLoaded", () => {
+    const inputField = document.getElementById("input");
+    inputField.addEventListener("keydown", function (e) {
+      if (e.code === "Enter") {
+        let input = inputField.value.trim();
+        input != "" && output(input);
+        inputField.value = "";
+      }
+    });
+  });
+  
+  function output(input) {
+    let product;
+  
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+  
+    text = text
+      .replace(/[\W_]/g, " ")
+      .replace(/ a /g, " ")
+      .replace(/i feel /g, "")
+      .replace(/whats/g, "what is")
+      .replace(/please /g, "")
+      .replace(/ please/g, "")
+      .trim();
+  
+    let comparedText = compare(userMessage, botReply, text);
+  
+    product = comparedText
+      ? comparedText
+      : alternative[Math.floor(Math.random() * alternative.length)];
+    addChat(input, product);
+  }
+  
+  function compare(triggerArray, replyArray, string) {
+    let item;
+    for (let x = 0; x < triggerArray.length; x++) {
+      for (let y = 0; y < replyArray.length; y++) {
+        if (triggerArray[x][y] == string) {
+          items = replyArray[x];
+          item = items[Math.floor(Math.random() * items.length)];
+        }
+      }
+    }
+    //containMessageCheck(string);
+    if (item) return item;
+    else return containMessageCheck(string);
+  }
+  
+  function containMessageCheck(string) {
+    let expectedReply = [
+      [
+        "Good Bye, dude",
+        "Bye, See you!",
+        "Dude, Bye. Take care of your health in this situation."
+      ],
+      ["Good Night, dude", "Have a sound sleep", "Sweet dreams"],
+      ["Have a pleasant evening!", "Good evening too", "Evening!"],
+      ["Good morning, Have a great day!", "Morning, dude!"],
+      ["Good Afternoon", "Noon, dude!", "Afternoon, dude!"]
+
+      [
+        "I noticed a gap in the market and saw an opportunity to innovate.",
+        "I've always been passionate about solving this particular problem.",
+        "I wanted to make a difference in this industry and saw a chance to do so with my startup."
+      ],
+      [
+        "Finding the right team members with the same vision has been challenging.",
+        "Raising capital and funding has been a major hurdle.",
+        "Navigating the market and competition has been tough."
+      ],
+      [
+        "I set clear goals and prioritize tasks based on their impact on those goals.",
+        "I regularly review my goals and adjust my focus as needed.",
+        "I surround myself with a supportive team that helps me stay focused."
+      ],
+      [
+        "Don't be afraid to take risks and learn from failures.",
+        "Build a strong network and seek mentorship from experienced entrepreneurs.",
+        "Stay persistent and believe in your vision, even when faced with challenges."
+      ],
+      [
+        "I envision expanding our product/service offerings and entering new markets.",
+        "I plan to scale the business and reach a wider customer base.",
+        "I aim to innovate and stay ahead of the curve in our industry."
+      ]
+    ];
+    let expectedMessage = [
+      ["bye", "tc", "take care"],["How do you see your startup evolving in the next few years?","What advice would you give to aspiring startup founders?","What advice would you give to aspiring startup founders?","How do you prioritize and stay focused on your goals?","What challenges have you faced while building your startup?","What challenges have you faced while building your startup?",
+        "What inspired you to start your startup?","What inspired you to start your startup?"
+      ],
+      ["night", "good night"],
+      ["evening", "good evening"],
+      ["morning", "good morning"],
+      ["noon"]
+    ];
+    let item;
+    for (let x = 0; x < expectedMessage.length; x++) {
+      if (expectedMessage[x].includes(string)) {
+        items = expectedReply[x];
+        item = items[Math.floor(Math.random() * items.length)];
+      }
+    }
+    return item;
+  }
+  function addChat(input, product) {
+    const mainDiv = document.getElementById("message-section");
+    let userDiv = document.createElement("div");
+    userDiv.id = "user";
+    userDiv.classList.add("message");
+    userDiv.innerHTML = `<span id="user-response">${input}</span>`;
+    mainDiv.appendChild(userDiv);
+  
+    let botDiv = document.createElement("div");
+    botDiv.id = "bot";
+    botDiv.classList.add("message");
+    botDiv.innerHTML = `<span id="bot-response">${product}</span>`;
+    mainDiv.appendChild(botDiv);
+    var scroll = document.getElementById("message-section");
+    scroll.scrollTop = scroll.scrollHeight;
+    voiceControl(product);
+  }
+  
